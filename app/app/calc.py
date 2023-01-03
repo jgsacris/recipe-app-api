1903a59fef96572b144a2ce3a6d746886cc65a92
Julio Garcia <jg@innovation.rocks>
2023-01-03 17:23:39 +0100

    add postgres deps

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     '''add x and y and return result'''
     return x + y
+
+
+def subtract(x, y):
+    '''subtract x from y and return result'''
+    return y - x
